0d983bf806adef19e97a85102495465c9da327b2
LestatDeLionkur <ownerofnothing@mail.ru>
2024-01-22 21:23:23 +0600

    итоговый вариант

--- blin.py
+++ blin.py
@@ -1,58 +1,168 @@
 import telebot
-import random
 import info
+import json
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
-# Создаём бота
+number_quest = 1
 API_TOKEN = '6540054720:AAEie0nYSXq442bG8df_a1N4If4Xh72YxMQ'
 bot = telebot.TeleBot(API_TOKEN)
+res = {"Анатолий Сергиевский": 0, "Товарищ Молоков": 0, "Фредди Трампер": 0, "Флоренс Васси": 0, "Арбитр": 0}
 
-# Изначально пользователей нет, пустой словарь
-user_data = {}
 
-# Начало работы, спрашиваем возраст
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет! Я умею рекомендовать игры.")
-    bot.send_message(message.chat.id, "Пожалуйста, скажи, сколько тебе лет?")
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(KeyboardButton("/help"))
+    markup.add(KeyboardButton("/go"))
+    bot.send_message(message.chat.id, info.start_text, reply_markup=markup)
 
-# Если возраст уже указан, рекомендуем игру
-@bot.message_handler(commands=['game'])
-def game(message):
-    # Получаем `user_id` пользователя
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add("/start")
+    keyboard.add("/help")
+    keyboard.add("/go")
+    keyboard.add("/restart")
+    bot.send_message(message.chat.id, info.help_text, reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['go'])
+def go(message):
+    global number_quest
     user_id = message.from_user.id
+    if number_quest in info.all_questions:
+        question_text = info.all_questions[number_quest]["question"]
+        options = info.all_questions[number_quest]["answers"].keys()
 
-    # Проверяем, что user_id пользователя есть в словаре
-    # Если нет -- просим прислать свой возраст
-    if user_id not in user_data:
-        bot.send_message(message.chat.id, "Пожалуйста, пришли сначала свой возраст.")
-    elif user_data[user_id]["age"] < 13:
-        bot.send_message(message.chat.id, random.choice(info.younger_than_13))
-    elif 13 <= user_data[user_id]["age"] < 18:
-        bot.send_message(message.chat.id, random.choice(info.younger_than_18))
+        keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+        for option in options:
+            keyboard.add(telebot.types.KeyboardButton(option))
+        bot.send_message(message.chat.id, question_text, reply_markup=keyboard)
+        save_progress(user_id, number_quest)
+        bot.register_next_step_handler(message, count_res)
     else:
-        bot.send_message(message.chat.id, random.choice(info.older_than_18))
 
-@bot.message_handler(commands=['hello'])
-def hello(message):
-    username = message.from_user.first_name
-    bot.send_message(message.chat.id, text=f"Привет, {username}!")
-# Запоминаем присланный возраст
-@bot.message_handler()
-def save_age(message):
-    # Проверяем, что возраст - число
-    if not message.text.isdigit():
-        bot.send_message(message.chat.id, "Пожалуйста, пришли свой возраст цифрами.")
-    else:
-        user_id = message.from_user.id
-        # Запоминаем присланный возраст в локальную переменную `age`
-        age = int(message.text)
-        # Сохраняем возраст пользователя в словарь по `user_id`
-        user_data[user_id] = {}
-        user_data[user_id]['age'] = age
-        # Сохраняем имя пользователя в словарь по `user_id`
-        user_data[user_id]['name'] = message.from_user.first_name
+        view_res(message)
 
-        bot.send_message(message.chat.id, "Отлично, я запомнил! Теперь можешь использовать команду /game")
 
-# Запускаем бота
+@bot.message_handler(commands=['restart'])
+def restart(message):
+    global number_quest
+    number_quest = 1
+    go(message)
+
+
+def count_res(message):
+    global number_quest
+    global res
+    if number_quest == 1:
+        if message.text == "Да":
+            res["Анатолий Сергиевский"] += 5
+            res["Товарищ Молоков"] += 5
+        if message.text == "Нет":
+            res["Флоренс Васси"] += 3
+        if message.text == "Смотря, какая цель, и какие средства":
+            res["Арбитр"] += 4
+    elif number_quest == 2:
+        if message.text == "Исполнение моих целей":
+            res["Анатолий Сергиевский"] += 5
+            res["Товарищ Молоков"] += 3
+            res["Фредди Трампер"] += 3
+            res["Флоренс Васси"] += 1
+            res["Арбитр"] += 1
+        if message.text == "Только я сам(-а)":
+            res["Анатолий Сергиевский"] += 2
+            res["Товарищ Молоков"] += 1
+            res["Фредди Трампер"] += 2
+            res["Флоренс Васси"] += 5
+            res["Арбитр"] += 1
+        if message.text == "Когда все играют по правилам":
+            res["Анатолий Сергиевский"] += 1
+            res["Фредди Трампер"] += 1
+            res["Флоренс Васси"] += 2
+            res["Арбитр"] += 4
+        if message.text == "Тепло и забота":
+            res["Анатолий Сергиевский"] += 1
+            res["Фредди Трампер"] += 5
+            res["Флоренс Васси"] += 2
+            res["Арбитр"] += 2
+    elif number_quest == 3:
+        if message.text == "Это окружающим нелегко со мной":
+            res["Товарищ Молоков"] += 3
+            res["Фредди Трампер"] += 5
+        if message.text == "Да, вполне":
+            res["Анатолий Сергиевский"] += 1
+            res["Товарищ Молоков"] += 3
+            res["Флоренс Васси"] += 4
+            res["Арбитр"] += 5
+        if message.text == "Достаточно сложно":
+            res["Анатолий Сергиевский"] += 4
+            res["Фредди Трампер"] += 3
+    elif number_quest == 4:
+        if message.text == "К сожалению, да":
+            res["Флоренс Васси"] += 4
+        if message.text == "Нет":
+            res["Анатолий Сергиевский"] += 5
+            res["Товарищ Молоков"] += 3
+        if message.text == "Зависит от человека":
+            res["Арбитр"] += 4
+        if message.text == "Нет, но хочу":
+            res["Фредди Трампер"] += 4
+    number_quest += 1
+    go(message)
+
+
+def save_progress(user_id, number_quest):
+    cur_progress = {str(user_id): number_quest}
+    try:
+        with open('progress.json', 'r') as file:
+            progress = json.load(file)
+        progress[str(user_id)] = number_quest
+        with open('progress.json', 'w') as file:
+            json.dump(progress, file)
+    except:
+        with open('progress.json', 'w') as file:
+            json.dump(cur_progress, file)
+
+
+def load_progress(user_id):
+    global number_quest
+    try:
+        with open('progress.json', 'r') as file:
+            number_quest = json.load(file)
+            return number_quest.get(str(user_id))
+    except FileNotFoundError:
+        return None
+
+
+def view_res(message):
+    itog = max(res, key=res.get)
+    if itog == "Фредди Трампер":
+        with open("фредди.jpg", "rb") as f:
+            keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+            keyboard.add("/restart")
+            bot.send_photo(message.chat.id, f, info.freddie, reply_markup=keyboard)
+    elif itog == "Анатолий Сергиевский":
+        with open("толя.jpg", "rb") as f:
+            keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+            keyboard.add("/restart")
+            bot.send_photo(message.chat.id, f, info.tolya, reply_markup=keyboard)
+    elif itog == "Товарищ Молоков":
+        with open("молоков.jpg", "rb") as f:
+            keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+            keyboard.add("/restart")
+            bot.send_photo(message.chat.id, f, info.molokov, reply_markup=keyboard)
+    elif itog == "Флоренс Васси":
+        with open("флоренс.jpg", "rb") as f:
+            keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+            keyboard.add("/restart")
+            bot.send_photo(message.chat.id, f, info.florence, reply_markup=keyboard)
+    elif itog == "Арбитр":
+        with open("арбитр.jpg", "rb") as f:
+            keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+            keyboard.add("/restart")
+            bot.send_photo(message.chat.id, f, info.arbitr, reply_markup=keyboard)
+
+
 bot.polling()
